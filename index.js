28f8fc5a22038354dd75c38c475bb6258ea475e0
Jo <134626490+Jcait@users.noreply.github.com>
2024-02-21 15:20:35 +0000

    Add player turn

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const game = (function() {
-    const gameBoard = ["","","","","","","","",""]
+    const gameBoard = [ "X","","",
+                        "X","X","",
+                        "","",""]
+    const _playerOne = "X"
+    const _playerTwo = "O"
+    let _currentTurn
 
-    function addSign(i) {
-        gameBoard[i] = "X"
+    function gameStart() {
+        const result = Math.floor(Math.random() *10 )
+        console.log(result)
+        playerTurn(result)
+    }
+    function playerTurn(result) {
+        if(result % 2 == 0) {
+            _currentTurncurrentTurn = _playerOne
+        } else {
+            _currentTurn = _playerTwo
+        }
+
     }
 
 
 
+    function addSign(i) {
+        gameBoard[i] = _currentTurn
+    }
+
+
     return {
         gameBoard,
+        gameStart,
         addSign
     }
 
