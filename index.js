40e1ffb211c7f4f3804972344d8ab96018fa88ff
Jo <134626490+Jcait@users.noreply.github.com>
2024-02-22 13:53:56 +0000

    Add dupe checjer

--- index.js
+++ index.js
@@ -14,13 +14,20 @@ const game = (function() {
         points: 0,
         moves: []
     })
+
+    const playerTwo = createPlayer({
+        name: "test2",
+        sign: "O",
+        points: 0,
+        moves: []
+    })
     const gameBoard = [ "X","","",
                         "X","X","",
                         "","",""]
 
     let _currentTurn
 
-
+// Starts game and randomly selects starting player
 
     function gameStart() {
         const result = Math.floor(Math.random() *10 )
@@ -30,13 +37,23 @@ const game = (function() {
     function playerTurn(result) {
         if(result % 2 == 0) {
             _currentTurn = playerOne
-        } else {
+        } else { 
 
-        return "test" }
+        _currentTurn = playerTwo}
 
     }
 
-    function addSign(i) {
+// checks if spots already taken before applying
+    function play(i) {
+        if(gameBoard[i] != ""
+        && _currentTurn.sign) {
+            console.log("This spot is taken")
+        } else {
+            addSign(_currentTurn, i)
+        }
+    }
+
+    function addSign(_currentTurn,i) {
         console.log(_currentTurn)
         gameBoard[i] = _currentTurn.sign
         _currentTurn.moves.push(i)
@@ -47,7 +64,7 @@ const game = (function() {
         gameBoard,
         playerOne,
         gameStart,
-        addSign, 
+        play, 
     }
 
 })()
